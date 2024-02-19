cd36d9139ca090103c756b7bfa7931bb459e1959
keyurpatel29 <keyurpatel8401@gmail.com>
2024-02-19 14:19:17 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className="App">
+    {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +19,7 @@ function App() {
          I am learning reactjs
         </a>
       </header>
-    </div>
+    </div> */}
 
     <div className= "App">
       <h1>Hello World!</h1>
